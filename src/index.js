b7f6f769a224000155674c2637b979c1b6220588
은배 <geb9211@gmail.com>
2022-01-18 23:32:52 +0900

    redux middleware 초기설정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules'
+import myLogger  from './middlewares/myLogger';
 import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(myLogger)));
 console.log(store.getState());
 
 ReactDOM.render(
